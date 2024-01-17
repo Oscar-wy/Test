3336038973e1c2a03a63678c8bc55c9d59605a79
Oscar-wy <156927877+Oscar-wy@users.noreply.github.com>
2024-01-17 15:00:29 +0000

    Simple Program

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
-list = [1,2,3,4,5]
+numbers = [1,2,3,4,7,8,9,44,3,11,545]
+found = False
+user = int(input("Enter A Number: "))
+for i in range(len(numbers)-1):
+    if user == numbers[i]:
+        print(f"{numbers[i]} Found")
+        found = True
+if found == False:
+    print(f"Not Found {user}!")
